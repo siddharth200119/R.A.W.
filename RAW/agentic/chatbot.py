6e35158db00018c0a0d5a23adfcf59b0888a74bd
Siddharth200119 <siddramnani@gmail.com>
2025-08-03 18:16:07 +0530

    updated this system to have a chatbot class

---
+++ RAW/agentic/chatbot.py
@@ -0,0 +1,50 @@
+from RAW.agentic.llms import LLM
+from RAW.models import Tool, Message
+from RAW.utils import Logger
+
+from typing import List, Optional
+
+class ChatBot():
+    def __init__(self, llm: LLM, tools: List[Tool] = [], logger: Logger = Logger(), system_prompt: str = ''):
+        self.logger = logger
+        if(not llm):
+            self.logger.error("Please provide and LLM")
+            raise RuntimeError("Please provide and LLM")
+        
+        self.llm = llm
+        self.tools = tools
+
+        if(len(tools) > 0 and 'tools' not in self.llm.capabilities):
+            self.logger.warning('the chosen LLM does not support tool calling skipping tools')
+            self.tools = []
+
+        self.messages: List[Message] = [
+            Message(
+                role='system',
+                content=system_prompt
+            )
+        ]
+
+    def __call__(self, message: Message, stream: bool = False, think: bool = False) -> Optional[Message]:
+        if(not message):
+            self.logger.error("Message cannot be empty")
+
+        self.messages.append(message)
+
+        if(stream):
+            return self._call_stream(think)
+        else:
+            return self._call_no_stream(think)
+        
+    
+    async def _call_stream(self, think: bool = False):
+        async for message in self.llm.chat(messages=self.messages, think=think, stream=True, tools=self.tools):
+            yield message
+
+        self.messages.append(message)
+        return
+
+    async def _call_no_stream(self, think: bool = False):
+        message = await self.llm.chat(messages=self.messages, think=think, stream=False, tools=self.tools)
+        self.messages.append(message)
+        return message
--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from RAW.utils import Logger
-from pathlib import Path
+from RAW.agentic.chatbot import ChatBot
 from RAW.agentic.llms import Ollama
-from RAW.models import Image, Message
-import uuid
+from RAW.models import Message
+from RAW.utils import Logger
+
 import asyncio
-from pathlib import Path
+import json
+
+logger = Logger()
+
+chatbot = ChatBot(
+    llm=Ollama(
+        capabilities=['tools', 'vision'],
+        logger=logger,
+        timeout=5000,
+        model='MrScarySpaceCat/gemma3-tools:4b'
+    ),
+    system_prompt='You are a friendly neighbourhood assistant',
+    logger=logger
+)
+
+message = Message(
+    role='user',
+    content='who are you'
+)
 
 async def main():
-    logger = Logger(log_file=Path("app.log"))
-    logger.info(content="app started")
-    # image = Image.from_path(Path('/home/siddharth/Downloads/556-200x300.jpg'))
-    llm = Ollama(model='gemma3:4b', logger=logger, capabilities=['vision'])
-    # async for chunk in llm.generate(prompt="whats in the image", images=[image], stream=True):
-    #     logger.info(f"{chunk.thought}{chunk.content}")
-
-    async for chunk in llm.chat(messages=[
-        Message(
-            content='what is in the image',
-            images=[],
-            role='user'
-        )
-    ], tools=[], stream=True):
-        print(chunk)
-
+    _message = await chatbot(message)
+    logger.debug(json.dumps(chatbot.messages, indent=2, default=str))
     logger.stop()
 
 if __name__ == '__main__':
-    asyncio.run(main=main())
+    asyncio.run(main())

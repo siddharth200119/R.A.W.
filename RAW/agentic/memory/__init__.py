54f51dc6ef76ef3cc67474a46724d440e0a16a98
Dhruvisen <dhruvisenjaliya05@gmail.com>
2025-08-11 19:18:21 +0530

    done nd dusted

--- RAW/agentic/memory/__init__.py
+++ RAW/agentic/memory/__init__.py
@@ -1,3 +1,5 @@
 from RAW.agentic.memory.shortterm import ShortTermMemory
+from RAW.agentic.memory.longterm.storage_sqlite import SQLiteStorage
+from RAW.agentic.memory.longterm.long_term_memory import LongTermMemory
 
-__all__ = [ShortTermMemory]
+__all__ = [ShortTermMemory,SQLiteStorage,LongTermMemory]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from RAW.agentic.chatbot import ChatBot
 from RAW.agentic.llms import Ollama
 from RAW.agentic.memory import ShortTermMemory
+from RAW.agentic.memory import LongTermMemory
+from RAW.agentic.memory import SQLiteStorage
 from RAW.models import Message
 from RAW.utils import Logger
 
@@ -16,13 +18,27 @@ llm = Ollama(
         model='qwen3:0.6b'
     )
 
-memory = ShortTermMemory(logger=logger, actionable_unit='length', actionable_quantity=3, summarize_to_system_prompt=True, llm=llm)
+short_term_memory = ShortTermMemory(logger=logger, actionable_unit='length', actionable_quantity=3, summarize_to_system_prompt=True, llm=llm)
+
+storage = SQLiteStorage(db_path="longterm_memory.db", logger=logger)
+
+
+long_term_memory = LongTermMemory(storage=storage, logger=logger)
+
 
 chatbot = ChatBot(
     llm=llm,
-    system_prompt='You are a friendly neighbourhood assistant',
+    user_id='4',
+system_prompt = """
+You are a helpful assistant. When the user requests to add, update, or delete long-term memory, respond by calling the appropriate tool `update_memory_tool` with correct parameters.  
+For example:  
+User: "Add a summary of our conversation"  
+You: call tool "update_memory_tool" with action "add" and messages containing the summary text.  
+If the user just asks general questions, answer normally without calling tools.
+""",
     logger=logger,
-    short_term_memory=memory
+    short_term_memory=short_term_memory,
+    long_term_memory=long_term_memory   
 )
 
 message = Message(
